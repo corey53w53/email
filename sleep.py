a9e8f60aac79da7e36eaca2b04d2563244e65c5e
Corey Wang <corefire2004@gmail.com>
2022-04-21 23:28:15 -0400

    round

--- sleep.py
+++ sleep.py
@@ -1,6 +1,5 @@
 import time
 init=time.time()
-print(time.time())
 #print every 15 seconds
 total_time_limit=30
 increment=5
@@ -8,5 +7,5 @@ fifteen=time.time()+increment
 while time.time()-init<total_time_limit:
     time_to_go=fifteen-time.time()
     time.sleep(time_to_go)
-    print(time.time()-init)
+    print(round(time.time()-init))
     fifteen+=increment
