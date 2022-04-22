9f1a1d86d2460f364d8c9c4e877678d5f4baf6f1
Corey Wang <corefire2004@gmail.com>
2022-04-21 23:26:23 -0400

    switched statements

--- sleep.py
+++ sleep.py
@@ -7,5 +7,5 @@ fifteen=time.time()+increment
 while True:
     time_to_go=fifteen-time.time()
     time.sleep(time_to_go)
-    fifteen+=increment
     print(time.time()-init)
+    fifteen+=increment
