9a8be865e35f4c8c295c1d824082dd73655dc7e5
Corey Wang <corefire2004@gmail.com>
2022-04-21 23:25:53 -0400

    increment

--- sleep.py
+++ sleep.py
@@ -1,10 +1,11 @@
 import time
 init=time.time()
-fifteen=time.time()+15
+print(time.time())
+#print every 15 seconds
+increment=5
+fifteen=time.time()+increment
 while True:
     time_to_go=fifteen-time.time()
     time.sleep(time_to_go)
-    fifteen+=15
-    time_passed=time.time()-init
-    print(time_passed)
-    print("end loop")
+    fifteen+=increment
+    print(time.time()-init)
