935dbd0a12036d3349aded12548056a3a32bb7a9
Corey Wang <corefire2004@gmail.com>
2022-04-21 23:27:22 -0400

    total time limit

--- sleep.py
+++ sleep.py
@@ -2,9 +2,10 @@ import time
 init=time.time()
 print(time.time())
 #print every 15 seconds
+total_time_limit=30
 increment=5
 fifteen=time.time()+increment
-while True:
+while time.time()-init<total_time_limit:
     time_to_go=fifteen-time.time()
     time.sleep(time_to_go)
     print(time.time()-init)
