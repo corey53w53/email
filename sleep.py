ec4f41eadb373c615f6e8e368d29ff77130b1dfd
Corey Wang <corefire2004@gmail.com>
2022-04-21 23:22:39 -0400

    fifteen

--- sleep.py
+++ sleep.py
@@ -1,5 +1,10 @@
 import time
 init=time.time()
+fifteen=time.time()+15
 while True:
-    time.sleep(0.5)
-    print(init-time.time())
+    time_to_go=fifteen-time.time()
+    time.sleep(time_to_go)
+    fifteen+=15
+    time_passed=time.time()-init
+    print(time_passed)
+    print("end loop")
