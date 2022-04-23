ac7c9ec2094ce8a886b68b538b84fd6fac30ca47
Corey Wang <corefire2004@gmail.com>
2022-04-23 19:57:00 -0400

    counts seconds

--- sleep.py
+++ sleep.py
@@ -7,11 +7,19 @@ total_time_in_mins=90
 total_time_limit=total_time_in_mins*60
 increment=inc_in_mins*60
 
+increment=1
+
 interval=time.time()+increment
 print("\n"*30)
 print(f'Starting timer for {total_time_in_mins} minutes.')
 while time.time()-init<total_time_limit:
     time.sleep(interval-time.time())
     print("\n"*30)
-    print(f'{round((time.time()-init)/60)} minutes')
+    time_diff=time.time()-init
+    mins,secs=divmod(time_diff,60)
+    mins=int(mins)
+    secs=round(secs)
+    if len(str(secs))==1:
+        secs="0"+str(secs)
+    print(f'{mins}:{secs}')
     interval+=increment
