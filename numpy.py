292225d6b73876b3a8844d3bebda66dddafb4c79
Corey Wang <corefire2004@gmail.com>
2022-04-26 00:04:27 -0400

    venv

---
+++ numpy.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3]); 
